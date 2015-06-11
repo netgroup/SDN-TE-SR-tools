77d488428093879bc03c3ae034d2f239b914fe21
Your Name <you@example.com>
2015-06-12 00:38:47 +0200

    uniformate unità di misura delle capacità dei link, ora sono in Mbps

--- parsers-generators/Graphml2nx.py
+++ parsers-generators/Graphml2nx.py
@@ -16,7 +16,7 @@ from distribuzioni import *
 L = 1000
 # Scale factor (Capacity and size are expressed in kb/s)
 S = 1000
-DEFAULT_LINK_CAPA = 60
+DEFAULT_LINK_CAPA = 155               # Le capacita' sono espresse in Mbps
 PERCENTAGE_NODES_CORE = 0.2
 
 pusher_cfg = {}
@@ -58,8 +58,8 @@ def parse_graphml(input_file_name, nx_topology, defa_node_type="OSHI-CR", defa_l
 	node_label_country_in_graphml=""
 	node_label_name_in_graphml=""
 	node_label_id_in_graphml=""
+	node_speed_units_in_graphml=""
 
-	
 	#Trova e assegna il codice degli attributi che ci interessano
 	for i in index_values_set:
 		if i.attrib['attr.name'] == "id" and i.attrib["for"] == "node":
@@ -70,10 +70,13 @@ def parse_graphml(input_file_name, nx_topology, defa_node_type="OSHI-CR", defa_l
 			node_label_country_in_graphml = i.attrib['id']   #d30
 		if i.attrib['attr.name'] == 'LinkSpeed' and i.attrib['for'] == 'edge':
 			node_speed_link_in_graphml = i.attrib['id']   #d39
-
+		if i.attrib['attr.name'] == 'LinkSpeedUnits' and i.attrib['for'] == 'edge':
+			node_speed_units_in_graphml = i.attrib['id']   #d39
+			
 
 	i=0
 	#Entra in ogni edge del file xml
+	
 	for e in edge_set:
 		
 		data_set = e.findall(ns + 'data')
@@ -82,14 +85,19 @@ def parse_graphml(input_file_name, nx_topology, defa_node_type="OSHI-CR", defa_l
 		#Entra negli attributi del campo edge
 		for d in data_set:
 			if d.attrib['key'] == node_speed_link_in_graphml:
-				#salva la capacita' del link
-				node_link_speed_value = re.sub(r'\s+', '', d.text)     # Da sistemare le unita' di misura delle capacita' della linea!!!!!!!
+				node_link_speed_value = float(re.sub(r'\s+', '', d.text))
 				cont = True
+			if d.attrib['key'] == node_speed_units_in_graphml:
+				node_speed_units_value = re.sub(r'\s+', '', d.text)
+				if node_speed_units_value == 'G':
+					node_link_speed_value = node_link_speed_value * 1000 
+				
 				
 		if cont is False:
-			node_link_speed_value=DEFAULT_LINK_CAPA  #Se nel xml non c'e' la capacita', di default l'ho messa a 60  
-				
-		id_node_link_speed_dict[i] = node_link_speed_value		
+			node_link_speed_value=DEFAULT_LINK_CAPA  #Se nel xml non c'e' la capacita', di default l'ho messa a 155 Mbps
+
+
+		id_node_link_speed_dict[i] = node_link_speed_value	
 
 		src_id = e.attrib['source']
 		dst_id = e.attrib['target']
@@ -97,14 +105,14 @@ def parse_graphml(input_file_name, nx_topology, defa_node_type="OSHI-CR", defa_l
 		#print "Link tra "+str(id_node_id_dict[src_id])+" e "+str(id_node_id_dict[dst_id])+" con capacita': "+str(id_node_link_speed_dict[i])
 
 		#Carico il link 
-		nx_topology.add_edge(int(src_id),int(dst_id), capacity = round(float(id_node_link_speed_dict[i])), allocated=0, flows=[])
+		nx_topology.add_edge(int(src_id),int(dst_id), capacity = id_node_link_speed_dict[i], allocated=0, flows=[])
 		
 		#GENERA COLLEGAMENTI CONTRARI A QUELLI SOPRA IN MODO DA CREARE LINK BIDIREZIONALI TRA I NODI CORE        
-		nx_topology.add_edge(int(dst_id),int(src_id), capacity = round(float(id_node_link_speed_dict[i])), allocated=0, flows=[])
+		nx_topology.add_edge(int(dst_id),int(src_id), capacity = id_node_link_speed_dict[i], allocated=0, flows=[])
 
 		i=i+1
 
-
+	
 	#Entra in ogni nodo del file xml
 	for n in node_set:
 
